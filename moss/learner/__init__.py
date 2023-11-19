fcca50c2fece4fa7b1cea404bf0919eb55391ea7
lanzhiyi <lanzhiyi.mail@qq.com>
2023-11-19 16:25:10 +0800

    Fix.

--- moss/learner/__init__.py
+++ moss/learner/__init__.py
@@ -3,4 +3,8 @@ from moss.learner.base import BaseLearner
 from moss.learner.impala import ImpalaLearner
 from moss.learner.ppo import PPOLearner
 
-__all__ = ["BaseLearner", "ImpalaLearner", "PPOLearner"]
+__all__ = [
+  "BaseLearner",
+  "ImpalaLearner",
+  "PPOLearner",
+]
--- moss/network/__init__.py
+++ moss/network/__init__.py
@@ -3,4 +3,9 @@ from moss.network.base import CommonNet, Network
 from moss.network.torso import DenseTorso
 from moss.network.value import DenseValue
 
-__all__ = ["CommonNet", "DenseTorso", "DenseValue", "Network"]
+__all__ = [
+  "CommonNet",
+  "DenseTorso",
+  "DenseValue",
+  "Network",
+]
