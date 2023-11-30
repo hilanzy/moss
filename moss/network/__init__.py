a6e82993f37442a2c30f2997822d7ecbd82aae37
lanzhiyi <lanzhiyi.mail@qq.com>
2023-11-30 22:27:44 +0800

    [Feature]Support RNN. Fixes #84

--- moss/network/__init__.py
+++ moss/network/__init__.py
@@ -1,11 +1,9 @@
 """Network."""
 from moss.network.base import CommonNet, Network
-from moss.network.torso import DenseTorso
 from moss.network.value import DenseValue
 
 __all__ = [
   "CommonNet",
-  "DenseTorso",
   "DenseValue",
   "Network",
 ]
---
+++ moss/network/torso/__init__.py
@@ -0,0 +1,7 @@
+"""Torso network."""
+from moss.network.torso.dense import DenseTorso
+
+
+__all__ = [
+    "DenseTorso",
+]
